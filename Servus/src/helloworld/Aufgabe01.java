491d02e46b00842fc9053a80c08ebcf0627f846e
Mira <Mira@MirasNotebook.speedport.ip>
2024-05-17 15:03:46 +0200

    Aufgabe 01

---
+++ Servus/src/helloworld/Aufgabe01.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class Aufgabe01 {
+
+	  public static void main(String[] args) {
+	System.out.println("Entero namo:");
+	String name;
+	name=SystemInReader.readString();
+	System.out.println("Hello " + name + ", wassuuuup ɔ(⊙.⊙)ɔ ");
+}
+}
